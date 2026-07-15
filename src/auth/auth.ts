5797777a5eaa9d8d51a3a79c216a1ca4e8a36313
vaghelaom <vaghelaom65@gmail.com>
2026-07-15 16:48:48 +0530

    project setup

---
+++ src/auth/auth.ts
@@ -0,0 +1,21 @@
+import 'dotenv/config';   // ← must be first line
+import { betterAuth } from 'better-auth';
+import { prismaAdapter } from 'better-auth/adapters/prisma';
+import { PrismaClient } from '../../generated/prisma/client';
+import { PrismaPg } from '@prisma/adapter-pg';
+
+const adapter = new PrismaPg({
+  connectionString: process.env.DATABASE_URL!,
+});
+const prisma = new PrismaClient({ adapter });
+
+export const auth = betterAuth({
+  database: prismaAdapter(prisma, {
+    provider: 'postgresql',
+  }),
+  emailAndPassword: {
+    enabled: true,
+  },
+});
+
+export type Auth = typeof auth;
